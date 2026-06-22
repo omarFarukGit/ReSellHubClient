6258396052cd63d1bf86cb148ed91cda519a7b2c
practiceTaqwa <muhammadabuhafz@gmail.com>
2026-06-22 14:41:27 +0600

    fix ts type error

--- src/lib/core/server.ts
+++ src/lib/core/server.ts
@@ -1,6 +1,6 @@
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
-export const serverFetch = async (path) => {
+export const serverFetch = async (path: string) => {
   const res = await fetch(`${baseUrl}${path}`);
 
   return res.json();
